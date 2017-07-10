e828c8870760f5f54c271889a55174c96e7bdf8a
Mathew057 <mathew0057@gmail.com>
2017-07-09 20:02:58 -0500

    animation for links

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,29 @@
  * @Email:  mathew0057@gmail.com
  * @Filename: main.js
  * @Last modified by:   Mathew
- * @Last modified time: 2017-07-05T15:44:22-05:00
+ * @Last modified time: 2017-07-09T19:52:49-05:00
  */
 
+ $(document).ready(function(){
+   // Add smooth scrolling to all links
+   $("a").on('click', function(event) {
+
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+       // Prevent default anchor click behavior
+       event.preventDefault();
+
+       // Store hash
+       var hash = this.hash;
+
+       // animate to the top of the element in 500ms
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 500, function(){
+
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+ });
